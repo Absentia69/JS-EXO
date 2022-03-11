94fc3514b41fb8a3c016c3b59ec42eaece4387dc
Absentia69 <amine.merah@yahoo.fr>
2022-03-11 19:53:43 +0100

    Merah Mohamed Lamine/ JS Homework 2

--- a.js
+++ a.js
@@ -1,16 +1,38 @@
-function sort(array){
+let a = [5,4,7,8,12,4,7,89,51,23,4,7];
+function removeElement (arr, elem){
+    let j=0;
+    var newArr=[];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]!=elem){
+            newArr[j] = arr[i];
+            j++
+        }
+    }
+    return newArr;
+}
+console.log(removeElement(a, 4));
+function sortArray(arr) {
     var x;
-        for (let i = 0; i < array.length; i++) {
-            for (let j = 0; j < array.length; j++) {
-                if(array[i]<=array[j]){
-                    x= array[i];
-                    array[i]=array[j]
-                    array[j]=x
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr.length; j++) {
+                if(arr[i]<=arr[j]){
+                    x= arr[i];
+                    arr[i]=arr[j]
+                    arr[j]=x
                 }
             }
         }
-    return array;
+    return arr;
 }
-var array = [-3,8,4,7,9,-4,-1];
-sort(array);
-console.log(array);
+console.log(sortArray(a));
+function removeDups(arr) {
+    sortArray(arr);
+    let newArr=[];
+    let j=0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]!=arr[i+1]) {
+            removeElement(arr, arr[i]);
+        }
+    }return newArr
+}
+console.log(removeDups(a));
